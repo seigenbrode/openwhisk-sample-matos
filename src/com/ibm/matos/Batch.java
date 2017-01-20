e1333c895a9ccb1349b0352837b6749898609074
wprichar <wprichar@us.ibm.com>
2017-01-20 10:55:33 -0600

    Update Batch.java

--- src/com/ibm/matos/Batch.java
+++ src/com/ibm/matos/Batch.java
@@ -112,7 +112,7 @@ public class Batch {
 
 		Utils.setJaasLocation();
                 logger.log(Level.INFO, "The value in Config.KAFKA_API_KEY_PROP = " + config.get(Config.KAFKA_API_KEY_PROP));
-		logger.log(Level.INFO, "The value in Config.kafkaApiKey = " + config.get(kafkaApiKey));
+		logger.log(Level.INFO, "The value in Config.kafkaApiKey = " + config.getProperty(kafkaApiKey));
 		String apiKey = config.get(Config.KAFKA_API_KEY_PROP);
 		Utils.updateJaasConfiguration(apiKey.substring(0, 16), apiKey.substring(16));
 
